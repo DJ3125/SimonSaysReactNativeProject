1cc02d48372528b46e1ddc839abc2eb32baf5e29
RC211 <rc211@d211.org>
2024-11-14 14:13:07 -0600

    Got Gyroscope working

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,5 @@
+import {useState} from "react";
+
 import { Image, StyleSheet, Platform } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
@@ -5,7 +7,14 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
+import { Gyroscope } from 'expo-sensors';
+
 export default function HomeScreen() {
+  // const [xTilt, setXTilt] = useState();
+  Gyroscope.addListener(function(data){
+    console.log(data.x);
+  });
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
